758875d37c19380095106f565c35bc1f7078e41c
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2024-03-25 23:57:17 +0500

    Username Field Added

--- app/models.py
+++ app/models.py
@@ -16,6 +16,9 @@ class User(models.Model):
     address = models.CharField(max_length=100)
     CNIC = models.CharField(max_length=14, unique=True)
     phone = models.CharField(max_length=13, unique=True)
+    username = models.CharField(max_length=150, null=False,default="")  # Added username field
+
+
 
 
 class Accounts(models.Model):
