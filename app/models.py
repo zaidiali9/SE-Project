e2961d71a9ed3456d361114d6c4d33f1287fd881
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2024-03-25 22:06:16 +0500

    Accounts Model Added

--- app/models.py
+++ app/models.py
@@ -14,3 +14,27 @@ class User(models.Model):
     address = models.CharField(max_length=100)
     CNIC = models.CharField(max_length=14, unique=True)
     phone = models.CharField(max_length=13, unique=True)
+
+class Accounts(models.Model):
+    SAVING = 'Saving'
+    CURRENT = 'Current'
+    FIXED_DEPOSIT = 'Fixed Deposit'
+
+    ACCOUNT_TYPE_CHOICES = [
+        (SAVING, 'Saving'),
+        (CURRENT, 'Current'),
+        (FIXED_DEPOSIT, 'Fixed Deposit'),
+    ]
+
+    ACTIVE = 'Active'
+    INACTIVE = 'Inactive'
+
+    STATUS_CHOICES = [
+        (ACTIVE, 'Active'),
+        (INACTIVE, 'Inactive'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=100, unique=True)
+    balance = models.FloatField()
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPE_CHOICES, default=CURRENT)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=ACTIVE)
