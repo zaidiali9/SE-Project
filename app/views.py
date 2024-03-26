b39755a3e341ee4e77d202cba95aa445084ecee7
ifraaa <ifraejaz07@gmail.com>
2024-03-26 11:58:45 +0500

    dashboard interface

--- app/views.py
+++ app/views.py
@@ -16,21 +16,32 @@ def login(request):
             user.phone = request.POST.get('Ph_no')
             user.save()
             messages.success(request, 'Account created successfully, please log in.')
-            return redirect('login')  # Use the name of your login route
+            return redirect('login')  
         else:
             uname = request.POST.get('Username_signin')
             pword = request.POST.get('Password_signin')
             try:
                 user = User.objects.get(name=uname)
-                if (pword == user.password):
-                    print("User is successfully logged in.")
-                    messages.success(request, "User is successfully logged in.")
+                if (pword == user.password):  
+                    request.session['user'] = user.id  
+                    return redirect('dashboard') 
                 else:
-                    print("Invalid login credentials.")
                     messages.error(request, "Invalid login credentials.")
             except User.DoesNotExist:
-                print("User does not exist.")
                 messages.error(request, "User does not exist.")
-            return redirect('login')  # Use the name of your login route
+            return redirect('login')  
 
     return render(request, 'authentication/index.html')
+
+
+
+def dashboard(request):
+    services = [
+        {'title': 'Bill Payments', 'icon': 'bi-wallet', 'url_name': 'bill_payments'},
+        {'title': 'Donations', 'icon': 'bi-gift', 'url_name': 'donations'},
+    ]
+    if 'user' in request.session: 
+        return render(request, 'dashboard/index.html' ,{'services': services})
+    else:
+        messages.error(request, "You must be logged in to view the dashboard.")
+        return redirect('login')
